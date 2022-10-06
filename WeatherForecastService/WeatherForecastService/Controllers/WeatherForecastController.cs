572802964a697d30b31134825f6477e7c50d002f
ZoNnon <maksimka.leukhin@mail.ru>
2022-10-06 11:16:39 +0300

    Add project files.

---
+++ WeatherForecastService/WeatherForecastService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherForecastService.Models;
+
+namespace WeatherForecastService.Controllers
+{
+    [Route("api/weather")]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly WeatherForecastHolder _holder;
+
+        public WeatherForecastController(WeatherForecastHolder holder)
+        {
+            _holder = holder;
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add([FromQuery] DateTime date, [FromQuery] int temperature)
+        {
+            _holder.Add(date, temperature);
+            return Ok();
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] DateTime date, [FromQuery] int temperature)
+        {
+            _holder.Update(date, temperature);
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] DateTime date)
+        {
+            _holder.Delete(date);
+            return Ok();
+        }
+
+        [HttpGet("get")]
+        public IActionResult Get([FromQuery] DateTime dateFrom, [FromQuery] DateTime dateTo)
+        {
+            return Ok(_holder.Get(dateFrom, dateTo));
+        }
+    }
+}
---
+++ WeatherForecastService/WeatherForecastService/Models/WeatherForecastHolder.cs
@@ -0,0 +1,40 @@
+﻿namespace WeatherForecastService.Models
+{
+    public class WeatherForecastHolder
+    {
+        private List<WeatherForecast> _forecasts;
+
+        public WeatherForecastHolder()
+        {
+            _forecasts = new List<WeatherForecast>();
+        }
+
+        public void Add(DateTime date, int temperatureC)
+        {
+            _forecasts.Add(new WeatherForecast() { Date = date, TemperatureC = temperatureC });
+        }
+
+        public bool Update(DateTime date, int temperatureC)
+        {
+            foreach(var item in _forecasts)
+            {
+                if (item.Date == date)
+                {
+                    item.TemperatureC = temperatureC;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<WeatherForecast> Get(DateTime dateFrom, DateTime dateTo)
+        {
+            return _forecasts.FindAll(item => item.Date >= dateFrom && item.Date <= dateTo);
+        }
+
+        public void Delete(DateTime date)
+        {
+            _forecasts.RemoveAll(item => item.Date == date);
+        }
+    }
+}
